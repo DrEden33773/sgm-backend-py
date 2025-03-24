ddebd4bffb5ec85d0647e9a34115a101e99fab85
Eden Wang <edwardwang33773@gmail.com>
2025-03-25 04:50:41 +0800

    事已至此, 先适配一下 neo4j 存储层吧

--- main.py
+++ main.py
@@ -2,10 +2,11 @@
 # ruff: noqa: F405
 
 from tests.ldbc_snb_interactive_sf01 import *
+from tests.simple_test_dataset import *
 
 if __name__ == "__main__":
-    # test_triangle_forest()
-    # test_more_triangle_forest()
+    test_triangle_forest()
+    test_more_triangle_forest()
 
     test_ic_1_on_sf01()
     # test_ic_11_on_sf01()
